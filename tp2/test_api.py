cf4b946adc2ac3ea6c737ec1135c79d8a8fc0833
leonorrebola9 <leonor.rebola@ubi.pt>
2026-06-03 12:19:22 +0100

    criação do requirements, venv e test_api

---
+++ tp2/test_api.py
@@ -0,0 +1,25 @@
+'''
+colocar no relatório: foi usado o modelo 2.5, pois o modelo 1.5 
+já não estava disponível na listagem de modelos da minha API gratuita
+'''
+
+import os
+from google import genai
+from dotenv import load_dotenv
+
+# 1. Carregar as variáveis de ambiente
+load_dotenv()
+client = genai.Client()
+
+print("Teste")
+try:
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents="Olá! Responde apenas 'API configurada com sucesso!' se estiveres a receber esta mensagem."
+    )
+    print("Sucesso! A resposta do modelo foi:")
+    print("---")
+    print(response.text)
+    print("---")
+except Exception as e:
+    print(f"Ups, deu erro no teste: {e}")
